1407a25b394f08c00f26a522cce0b9c0fb4a01c2
EmilyYang47 <eyang4@bu.edu>
2025-07-06 17:18:55 -0400

    Modified styling and finished basic functions of the calculator

--- mp1/proj/projects.js
+++ mp1/proj/projects.js
@@ -3,6 +3,61 @@ let numTwo = document.getElementById("two");
 let result = document.getElementById("output");
 
 function doAdd() {
-  console.log("I am working");
-  result.innerHTML = `${String(Number(numOne.value) + Number(numTwo.value))}`;
+  value = Number(numOne.value) + Number(numTwo.value);
+
+  if (value < 0) {
+    result.innerHTML = `<h4 id="negative">${String(value)}</h4>`;
+  } else {
+    result.innerHTML = `<h4>${String(value)}</h4>`;
+  }
+}
+
+function doSubtract() {
+  value = Number(numOne.value) - Number(numTwo.value);
+
+  if (value < 0) {
+    result.innerHTML = `<h4 id="negative">${String(value)}</h4>`;
+  } else {
+    result.innerHTML = `<h4>${String(value)}</h4>`;
+  }
+}
+
+function doMultiply() {
+  value = Number(numOne.value) * Number(numTwo.value);
+
+  if (value < 0) {
+    result.innerHTML = `<h4 id="negative">${String(value)}</h4>`;
+  } else {
+    result.innerHTML = `<h4>${String(value)}</h4>`;
+  }
+}
+
+function doDivide() {
+  value = Number(numOne.value) / Number(numTwo.value);
+
+  if (value < 0) {
+    result.innerHTML = `<h4 id="negative">${String(value)}</h4>`;
+  } else {
+    result.innerHTML = `<h4>${String(value)}</h4>`;
+  }
+}
+
+function doPower() {
+  let value = 1;
+
+  for (let i = 0; i < Number(numTwo.value); i++) {
+    value = Number(numOne.value) * value;
+  }
+
+  if (value < 0) {
+    result.innerHTML = `<h4 id="negative">${String(value)}</h4>`;
+  } else {
+    result.innerHTML = `<h4>${String(value)}</h4>`;
+  }
+}
+
+function doClear() {
+  numOne.value = "";
+  numTwo.value = "";
+  result.innerHTML = `${String()}`;
 }
